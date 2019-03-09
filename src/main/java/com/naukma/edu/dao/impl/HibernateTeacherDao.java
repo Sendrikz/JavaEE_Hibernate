9915f4bf8f77577032b84852fc08834cf5b4fe97
Sendrikz <yuryeva.olga99@gmail.com>
2019-03-10 01:28:20 +0200

    Initial commit

---
+++ src/main/java/com/naukma/edu/dao/impl/HibernateTeacherDao.java
@@ -0,0 +1,35 @@
+package com.naukma.edu.dao.impl;
+
+import com.naukma.edu.dao.TeachersDao;
+import com.naukma.edu.enteties.Teacher;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HibernateTeacherDao implements TeachersDao {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public HibernateTeacherDao(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    private Session currentSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    public void addTeacher(Teacher teacher) {
+        currentSession().save(teacher);
+    }
+
+    public Teacher getTeacherById(int id) {
+        return (Teacher) currentSession().get(Teacher.class, id);
+    }
+
+    public void saveTeacher(Teacher teacher) {
+        currentSession().update(teacher);
+    }
+}
